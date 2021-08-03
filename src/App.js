ba84e76950963a7632662053be603f3cac887001
HAEROOL <gofn080776@gmail.com>
2021-08-03 17:01:27 +0900

    feat : Add represent lecture ranking layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Banner from './component/Banner/Banner'
 import styled from 'styled-components';
+import LectureRanking from './component/LectureRanking/LectureRanking';
 const TotalPage = styled.div`
   width : 100vw;
   height : 100vh;
   display : flex;
-  // flex-direction : row;
+  flex-direction : column;
   justify-content : center;
 `
 function App() {
   return (
     <TotalPage>
-      <Banner/>
+      {/* <Banner/> */}
+      <LectureRanking/>
     </TotalPage>
   );
 }
---
+++ src/component/LectureRanking/LectureRankingStyle.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const LectureRankingWrapper = styled.div`
+    width : 464px;
+    height : 396px;
+    border-radius : 8px;
+    border : solid 1px #eeeeee;
+`
+export const DepartmentSelector = styled.ul`
+    display : flex;
+    padding : 0px;
+    margin : 0px auto;
+    justify-content : space-around;
+    align-items : center;
+    height : 40px;
+    font-size : 12px;
+    list-style : none;
+    border-bottom : 1px solid #eeeeee;
+`
+
+export const DepartmentName = styled.div`
+    heigth : 40px;
+    cursor : pointer;
+    color : ${props => props.isClicked?'#238bfe':'#999999'}
+`
