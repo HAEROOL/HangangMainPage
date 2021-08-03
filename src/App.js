79f09d9623670cfadbcc9930f066b5003b3f2d23
HAEROOL <gofn080776@gmail.com>
2021-08-04 05:43:20 +0900

    feat : Add Illust of hangang main page and catchphrase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Banner from './component/Banner/Banner'
 import styled from 'styled-components';
+import MainPageIllust from './component/Banner/MainPageIllust';
 const TotalPage = styled.div`
   width : 100vw;
   height : 100vh;
   display : flex;
-  // flex-direction : row;
-  justify-content : center;
+  flex-direction : column;
+  // justify-content : center;
+  align-items : center;
 `
 function App() {
   return (
     <TotalPage>
       <Banner/>
+      <MainPageIllust/>
     </TotalPage>
   );
 }
