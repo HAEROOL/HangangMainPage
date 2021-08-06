e582342177cd862830273872c9351376d58d4055
HAEROOL <gofn080776@gmail.com>
2021-08-06 16:30:19 +0900

    Merge branch 'feature-MainpageBanner' into develop

---
+++ src/component/MainMenues/Department/DepartmentStyle.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+export const DepartmentBrowserWrapper = styled.div`
+    width : 1135px;
+    height : 120px;
+    margin : 0 auto;
+    margin-bottom : 32px;
+`
+export const DepartmentListWrapper = styled.div`
+    display : flex;
+    justify-content : space-between;
+`
+export const Department = styled.a`
+    position : relative;
+    text-align : center;
+    object-fit: contain;
+    font-size : 12px;
+`
+export const DepartmentImages = styled.img`
+    cursor : pointer;
+    height : 87px;
+    width : 100px;
+    border-radius : 10px;
+`
+export const DepartmentName = styled.div`
+    position : absolute;
+    top : 50%;  
+    color : #ffffff;
+`
+export const ComponentName = styled.div`
+    font-size : 16px;
+    text-align : left;
+    margin-bottom : 16px;
+`
