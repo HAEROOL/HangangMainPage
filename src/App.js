d0b78fe38724eefa61f70a477035a25df6534918
HAEROOL <gofn080776@gmail.com>
2021-08-06 14:34:33 +0900

    feat : Apply NotoSansKR font as global font

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import Banner from './component/Banner/Banner'
 import styled from 'styled-components';
 import MainPage from './pages/MainPage';
 import MainPageFooter from './component/Footer/MainPageFooter'
+import './App.css';
+
 const TotalPage = styled.div`
   display : flex;
+  font-family: 'Noto Sans KR';
   flex-direction : column;
   justify-content : center;
   align-items : center;
 `
 function App() {
   return (
-    <TotalPage>
-      <Banner/>
-      <MainPage/>
-      <MainPageFooter/>
-    </TotalPage>
+
+      <TotalPage>
+        <Banner/>
+        <MainPage/>
+        <MainPageFooter/>
+      </TotalPage>
+
   );
 }
 
