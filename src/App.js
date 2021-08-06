ccf04a2b065e552e717744ab1ac8da2838dcf72d
HAEROOL <gofn080776@gmail.com>
2021-08-06 18:01:39 +0900

    fix : Change MenuWrapper not shrinking when browser width is less than 600px

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
   return (
       <TotalPage>
         <Banner/>
-        <MainPage/>
-        <MainPageFooter/>
+        {/* <MainPage/> */}
+        {/* <MainPageFooter/> */}
       </TotalPage>
 
   );
