b2006c8a53aebc94a298a01e397b3d66ec04030c
HAEROOL <gofn080776@gmail.com>
2021-08-09 18:22:33 +0900

    refactor : Set layout of mainmenues of mainpage

--- src/component/MainMenues/Department/Department.js
+++ src/component/MainMenues/Department/Department.js
@@ -14,7 +14,6 @@ const DepartmentList = [Elective, HRD, MechanicalEngineerging,DesignArchitectura
 const DepartmentNameList = ['교양학부','HRD학과','기계공학부','디자인 건축 공학부','메카트로닉스공학부','산업경영학부','에너지신소재 화학 공학부','전기전자통신 공학부','컴퓨터공학부','융합학과']
 
 function DepartmentBrowser(){
-    ;
     return (
         <DepartmentBrowserWrapper>
             <ComponentName>학부별 탐색</ComponentName>
