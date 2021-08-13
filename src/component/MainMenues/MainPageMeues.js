f429637c23b6b1746b2a06669fff89253200bafe
HAEROOL <gofn080776@gmail.com>
2021-08-13 18:07:26 +0900

    docs : Rename MainPageMenues.style.js
    Rename MainPageMenuesStyle.js to MainPageMenues.style.js

--- src/component/MainMenues/MainPageMeues.js
+++ src/component/MainMenues/MainPageMeues.js
@@ -3,7 +3,7 @@ import CurrentLecture from './CurrentLecture/CurrentLecture'
 import TimeTable from './TimeTable/TimeTable'
 import RecommendationLectureMaterial from './RecommendationLecutreMaterial/RecommendationLectureMaterial'
 import LectureRanking from './LectureRanking/LectureRanking'
-import { MainMenuBrowserWrapper,FifthMaterial, FirstFourthThirdFifthWrapper, FirstMaterial, FourthMaterial, FourthThirdFifthWrapper, MainPageMenuWrapper, SecondMaterial, ThirdMaterial, ThridFifthWrapper } from './MainPageMenuesStyle'
+import { MainMenuBrowserWrapper,FifthMaterial, FirstFourthThirdFifthWrapper, FirstMaterial, FourthMaterial, FourthThirdFifthWrapper, MainPageMenuWrapper, SecondMaterial, ThirdMaterial, ThridFifthWrapper } from './MainPageMenues.style'
 import DepartmentBrowser from './Department/Department'
 function MainPageMenu(){
     return(
