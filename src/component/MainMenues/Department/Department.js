89df3dcf4f347c06a969b002a08b7062c76417ac
HAEROOL <gofn080776@gmail.com>
2021-08-18 21:39:12 +0900

    docs : Change Link tag to NavLink tag

--- src/component/MainMenues/Department/Department.js
+++ src/component/MainMenues/Department/Department.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import { DepartmentBrowserWrapper,DepartmentListWrapper, Department, DepartmentName, ComponentName, Scroll } from './Department.style'
+import { StyledLink,DepartmentBrowserWrapper,DepartmentList, Department, DepartmentName, ComponentName, DepartmentListScroll } from './Department.style'
 import { DEPARTMENT_LIST } from '../../../static/indexPage/departmentList'
-import { Link } from 'react-router-dom'
+
 function DepartmentBrowser(){
     return (
         <DepartmentBrowserWrapper>
             <ComponentName>학부별 탐색</ComponentName>
-            <Scroll>
-            <DepartmentListWrapper>
-                    {DEPARTMENT_LIST.map((department,id) =>
-                    <Link to='/dummy'  style={{textDecoration:'none'}} key={id}>
+            <DepartmentListScroll>
+            <DepartmentList>
+                    {DEPARTMENT_LIST.map((department) =>
+                    <StyledLink to='/lectures' key={department.id}>
                         <Department url={department.src}>
-                            <DepartmentName>{department.title}</DepartmentName>
+                            <DepartmentName>{department.fullTitle}</DepartmentName>
                         </Department>
-                    </Link>
+                    </StyledLink>
                     )}
-            </DepartmentListWrapper>
-            </Scroll>
+            </DepartmentList>
+            </DepartmentListScroll>
         </DepartmentBrowserWrapper>
     )
 }
