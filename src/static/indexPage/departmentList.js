4ad3bc71d8fead628180bed9fdc1510ceab58ba7
HAEROOL <gofn080776@gmail.com>
2021-08-18 21:38:32 +0900

    docs : Add fullTitle to departmentList.js
    Add fullTitle to departmentList.js for department browser

--- src/static/indexPage/departmentList.js
+++ src/static/indexPage/departmentList.js
@@ -10,14 +10,14 @@ import ComputerEngineering from '../../assets/images/Department/ComputerEngineer
 import Fusion from '../../assets/images/Department/Fusion.jpg'
 
 export const DEPARTMENT_LIST = [
-    {title:'교양',id:10,src:Elective},
-    {title:'HRD',id:9,src:HRD},
-    {title:'기계',id:1,src:MechanicalEngineerging},
-    {title:'디·건',id:5,src:DesignArchitecturalEngineering},
-    {title:'메카',id:3,src:MechatronicsEngineering},
-    {title:'에신화',id:6,src:IndustrialManagement},
-    {title:'산경',id:7,src: EnergyNewMaterialsChemicalEngineering},
-    {title:'전전통',id:4,src:ElectricalElectronicCommunication},
-    {title:'컴공',id:2,src: ComputerEngineering},
-    {title:'융합',id:8,src:Fusion}
+    {title:'교양',fullTitle:'교양학부',id:10,src:Elective},
+    {title:'HRD',fullTitle:'HRD학과',id:9,src:HRD},
+    {title:'기계',fullTitle:'기계공학부',id:1,src:MechanicalEngineerging},
+    {title:'디·건',fullTitle:'디자인 건축 공학부',id:5,src:DesignArchitecturalEngineering},
+    {title:'메카',fullTitle:'메카트로닉스 공학부',id:3,src:MechatronicsEngineering},
+    {title:'에신화',fullTitle:'산업경영학부',id:6,src:IndustrialManagement},
+    {title:'산경',fullTitle:'에너지신소재 화학 공학부',id:7,src: EnergyNewMaterialsChemicalEngineering},
+    {title:'전전통',fullTitle:'전기전자통신 공학부',id:4,src:ElectricalElectronicCommunication},
+    {title:'컴공',fullTitle:'컴퓨터공학부',id:2,src: ComputerEngineering},
+    {title:'융합',fullTitle:'융합학과',id:8,src:Fusion}
 ]
